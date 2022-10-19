845f736f84a11346e07d1b694f1008bfe268cde5
KSKRLIFE <konduru@pdx.edu>
2022-10-19 12:28:17 -0700

    Added

--- hw2/04-api.js
+++ hw2/04-api.js
@@ -4,4 +4,16 @@ const url = 'https://restcountries.com/v3.1/all';
 
 // Add your code here
 
+
+async function getData(url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    var list = document.getElementById('results');
+    data.forEach(element => {
+        var data = element.name.common + " - " + element.population.toLocaleString();
+        var entry = document.createElement('li');
+        entry.appendChild(document.createTextNode(data));
+        list.appendChild(entry);
+    });            
+}
 getData(url);
