a77690ccda4b1868706ff5518901afd9940448d7
KSKRLIFE <konduru@pdx.edu>
2022-10-19 12:28:17 -0700

    Added updated hw2 files

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -1,15 +1,53 @@
 /** Exercise 01 - Coins **/
 
-const calculateChange = (input) => {
-  // Add your code here
-};
 
-// Sample Test Cases
-console.log(calculateChange(4.62));
-// $4.62 ==> 4 dollars, 2 quarters, 1 dime, 2 pennies
-console.log(calculateChange(9.74));
-// $9.74 ==> 9 dollars, 2 quarters, 2 dimes, 4 pennies
-console.log(calculateChange(0.16));
-// $0.16 ==> 1 dime, 1 nickel, 1 penny
-console.log(calculateChange(15.11));
-// $15.11 ==> Error: the number is too large
+console.log("Input the number");
+process.stdin.setEncoding('utf8');
+var number;
+process.stdin.on('readable', function(number) {
+    number = process.stdin.read();
+    
+    if (number !== null && 0<number && number<=10) 
+    {
+        let text=number.toString();
+        const myarray=text.split(".");
+        let num1=myarray[0];
+        let num2=myarray[1];
+        // 15.75 
+        let quarter=0; //25's
+        let dime=0; //10's
+        let pennie=0; // 1's
+
+        let n=num2;
+        console.log(typeof(n/25));
+        if(n)
+        {
+            if(n/25!=0)
+            {
+                quarter+=Math.trunc(n/25);
+                n=n%25
+            }
+            if(n/10!=0)
+            {
+                dime+=Math.trunc(n/10);
+                n=n%10;
+            }
+            if(n)
+            {
+                pennie+=n;
+            }
+        }
+
+        console.log((parseInt(num1)?parseInt(num1)+' dollars'+' ':'')+ (quarter? quarter+' quarters'+' ':'')+(dime? dime+' dimes'+' ': '')+(pennie?pennie+' pennies'+'':''));
+        process.exit();
+
+    }
+    else{
+        console.log("Enter a valid number");
+    }
+    }
+)
+
+    
+
+
--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,3 +1,19 @@
 /** Exercise 03 - Form **/
 
 // Add your code here
+function form1(event){
+    let a= document.getElementById('name').value;
+    let b = document.getElementById('email').value;
+    let c = document.getElementById('Feedback').value;
+    //console.log("========Form Submitted========")
+    console.log("Name :", a);
+    console.log("Email ID :", b);
+    console.log( c?'Feedback : '+ c:("Feedback : No feedback was submitted"));
+    if(document.getElementById('newsletter').checked){
+        console.log("Newsletter : Yes, I would like to join the newsletter.");
+    }
+    else{
+        console.log("Newsletter : No, thank you.")
+    } 
+
+}
