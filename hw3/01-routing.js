3f954d1049a96ac156baf3f5c6e46c8a6d99151b
KSKRLIFE <konduru@pdx.edu>
2022-11-04 01:16:42 -0700

    01-routing
    Updated port numbers

--- hw3/01-routing.js
+++ hw3/01-routing.js
@@ -1,17 +1,17 @@
 const http = require('http');
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5001;
 
-// http://localhost:5002/welcome should return a status code 200 with a welcome message of your choice in html format
+// http://localhost:5001/welcome should return a status code 200 with a welcome message of your choice in html format
 
-// http://localhost:5002/redirect should redirect the request to '/redirected' by using 302 as the status code / the redirected page should return a redirected message of your choice
+// http://localhost:5001/redirect should redirect the request to '/redirected' by using 302 as the status code / the redirected page should return a redirected message of your choice
 
-// http://localhost:5002/cache should return 'this resource was cached' in html format and set the cache max age to a day
+// http://localhost:5001/cache should return 'this resource was cached' in html format and set the cache max age to a day
 
-// http://localhost:5002/cookie should return 'cookies… yummm' in plain text and set 'hello=world' as a cookie
+// http://localhost:5001/cookie should return 'cookies… yummm' in plain text and set 'hello=world' as a cookie
 
-// http://localhost:5002/check-cookies should return 'yes' / 'no' in plain text depending on whether the browser has the 'hello' cookie
+// http://localhost:5001/check-cookies should return 'yes' / 'no' in plain text depending on whether the browser has the 'hello' cookie
 
-// For other routes, such as http://localhost:5002/other, this exercise should return a status code 404 with '404 - page not found' in html format
+// For other routes, such as http://localhost:5001/other, this exercise should return a status code 404 with '404 - page not found' in html format
 
 const server = http.createServer((req, res) => {
   const routes = [
@@ -65,7 +65,7 @@ const server = http.createServer((req, res) => {
     res.end();
   }
   else if (req.url === "/cookie") {
-    var cookie = require('cookie');
+  var cookie = require('cookie');
   res.setHeader("Set-Cookie", cookie.serialize('hello', 'world', {httpOnly: true})
   );
   res.writeHead(200, { "content-type": "text/plain" });
